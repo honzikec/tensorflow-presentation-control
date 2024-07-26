085ca45a6965e28d07d1bc378a88c02c3c56cf4a
jesus padron <jesuspadron4497@gmail.com>
2024-07-26 16:38:03 +0200

    video script

--- webcam.js
+++ webcam.js
@@ -1,27 +1,13 @@
-import './style.css'
-
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <video id="webcam" autoplay></video>
-  </div>
-`
-
 document.addEventListener("DOMContentLoaded", function() {
     const video = document.getElementById('webcam');
-    console.log("WORKING?", video)
-
 
     // Check if the browser supports getUserMedia
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         // Request access to the webcam
         navigator.mediaDevices.getUserMedia({video: true})
             .then(function (stream) {
-                console.log('stream')
                 // Set the video source to the webcam stream
                 video.srcObject = stream;
-            }).catch(error => {
-                console.log('error', error)
             })
     }
 });
